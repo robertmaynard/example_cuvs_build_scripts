5fc7c1355bd37465aa61e3321fbae4d1563c54d4
Robert Maynard <rmaynard@nvidia.com>
2025-09-08 09:01:18 -0400

    Automatically build and install cuvs when running container

--- build_libcuvs.sh
+++ build_libcuvs.sh
@@ -10,5 +10,6 @@ docker run -it --rm --ipc=host \
   -v $(pwd)/:/code/.out \
   -w /code/ \
   --platform linux/$1 \
-  libcuvs_base_$1
+  libcuvs_base_$1 \
+  /bin/bash /code/scripts/build_and_install.sh
 
