4b8202b9d38940367ad8e462d522d43f58814c22
Robert Maynard <rmaynard@nvidia.com>
2025-10-30 15:54:59 -0400

    Update logic to support CUDA 12 and 13 builds

--- build_libcuvs.sh
+++ build_libcuvs.sh
@@ -9,6 +9,6 @@ docker run -it --rm --ipc=host \
   -v $(pwd)/:/code/.out \
   -w /code/ \
   --platform linux/$1 \
-  libcuvs_base_$1 \
+  libcuvs_base_$2_$1 \
   /bin/bash /code/scripts/build_and_install.sh
 
--- scripts/build_and_install.sh
+++ scripts/build_and_install.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 # Build the cuvs C++ library and dependencies as static
-cmake -S /code/cuvs/cpp -B "/code/cuvs/cpp/build/${TARGET_ARCH}" \
+cmake -S /code/cuvs/cpp -B "/code/cuvs/cpp/build/${CUDA_MAJOR}/${TARGET_ARCH}" \
       -DCMAKE_CUDA_FLAGS=-w \
       -DCMAKE_CUDA_ARCHITECTURES=RAPIDS \
       -DBUILD_SHARED_LIBS=OFF \
@@ -12,15 +12,15 @@ cmake -S /code/cuvs/cpp -B "/code/cuvs/cpp/build/${TARGET_ARCH}" \
       -DBUILD_TESTS=OFF \
       -DBUILD_SHARED_LIBS=ON \
       -DCUVS_STATIC_RAPIDS_LIBRARIES=ON
-cmake --build "/code/cuvs/cpp/build/${TARGET_ARCH}" -j16
+cmake --build "/code/cuvs/cpp/build/${CUDA_MAJOR}/${TARGET_ARCH}" -j16
 
-cmake -S /code/cuvs/c -B "/code/cuvs/c/build/${TARGET_ARCH}" \
+cmake -S /code/cuvs/c -B "/code/cuvs/c/build/${CUDA_MAJOR}/${TARGET_ARCH}" \
       -DCMAKE_CUDA_FLAGS=-w \
       -DCUVSC_STATIC_CUVS_LIBRARY=ON \
-      -DCMAKE_PREFIX_PATH="/code/cuvs/cpp/build/${TARGET_ARCH}" \
+      -DCMAKE_PREFIX_PATH="/code/cuvs/cpp/build/${CUDA_MAJOR}/${TARGET_ARCH}" \
       -DBUILD_TESTS=OFF
 
-cmake --build "/code/cuvs/c/build/${TARGET_ARCH}" -j16
-cmake --install "/code/cuvs/c/build/${TARGET_ARCH}" --prefix /tmp/rapids/
+cmake --build "/code/cuvs/c/build/${CUDA_MAJOR}/${TARGET_ARCH}" -j16
+cmake --install "/code/cuvs/c/build/${CUDA_MAJOR}/${TARGET_ARCH}" --prefix /tmp/rapids/
 
-tar czf /code/.out/libcuvs_$TARGET_ARCH.tar.gz -C /tmp/rapids/ .
+tar czf /code/.out/libcuvs_${CUDA_MAJOR}_${TARGET_ARCH}.tar.gz -C /tmp/rapids/ .
